06b17525a1bc5d30c7730b4b968e44f11ea21c9a
MarcoGranata9 <granatam9900@gmail.com>
2023-11-29 12:57:38 +0100

    add: appheader componet event

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script>
+import axios from "axios";
+import {store} from "./store.js";
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
   data() {
     return {
+      store,
+    }
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    searchFunction() {
       
     }
-  }
+  },
 }
 </script>
 
 <template>
-
+  <AppHeader @searchText="searchFunction"/>  
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>BOOLFLIX</h1>
+        <input type="text" v-model="store.searchText">
+        <button @click="$emit('searchText')">Cerca</button>
+    </header>    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
