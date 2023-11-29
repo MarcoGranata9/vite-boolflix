8b00d4eed751e3b802ae46dbe6f6a3754e168c28
MarcoGranata9 <granatam9900@gmail.com>
2023-11-29 19:05:08 +0100

    add: falgs wip

--- src/components/AppMovieList.vue
+++ src/components/AppMovieList.vue
@@ -5,6 +5,16 @@ export default {
         return {
             store,
         }
+    },
+    methods: {
+        getFlag(index) {
+            let language = this.store.movieArray[index].original_language;
+            if (language === "it") {
+                return "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/220px-Flag_of_Italy.svg.png"   
+            } else {
+                return language
+            }
+        }
     }
 }
 </script>
@@ -12,12 +22,20 @@ export default {
 <template>
     <section>
         <ul>
-            <li v-for="movie in this.store.movieArray">{{ movie.title }} {{ movie.original_title }} {{ movie.original_language }} {{ movie.vote_average }}</li>
+            <li v-for="(movie, index) in this.store.movieArray" :key="movie.id">
+                <span>{{ movie.title }}</span> 
+                <span>{{ movie.original_title }}</span>
+                <img :src="getFlag(index)" alt="">
+                <span>{{ getFlag(index) }}</span>
+                <span>{{ movie.vote_average }}</span>
+            </li>
             <li>ciao</li>
         </ul>        
     </section>
 </template>
 
 <style lang="scss" scoped>
-
+    img {
+        width: 50px;
+    }
 </style>
